314b31529266e930e4a6762e522cde73eab696db
temPLAY333 <t.bourguet@alumno.um.edu.ar>
2024-03-19 19:28:28 -0300

    hecho

--- entidades/Main.java
+++ entidades/Main.java
@@ -1,2 +1,11 @@
-package Entidades;public class Main {
+package entidades;
+
+public class Main {
+    static Ingrediente ingre1 = new Ingrediente("tomate",5);
+    static Ingrediente ingre2 = new Ingrediente("zanahoria",3);
+
+    public static void main(String args[]) {
+        System.out.println(ingre1.toString());
+        System.out.println(ingre2.toString());
+    }
 }
